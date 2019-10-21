052e11c607fc4b1913a4167f01e3ab03c56b76fc
Yanbitten <polorotova.yana@gmail.com>
2019-10-21 15:02:27 +0600

    rd1a

--- raid1a.go
+++ raid1a.go
@@ -1,32 +1,60 @@
-package student
+package main
 
-import "github.com/01-edu/z01"
+import (
+    "github.com/01-edu/z01"
+)
 
-func Raid1a(x, y int) {
-	if x < 0 || y < 0 {
-		return
-	}
-	for p := 0; p < y; p++ {
-		if p == 0 || p == y-1 {
-			for n := 0; n < x; n++ {
-				if n == 0 || n == x-1 {
-					z01.PrintRune('o')
-				} else {
-					z01.PrintRune('-')
-				}
-			}
-			z01.PrintRune(10)
+func Raid1c(x, y int) {    // задаем ширину и длину 
+   if y >= 1 {             //условие: если длина больше или ровна 1
+        if x > 0 {         //условаие:  если ширина больше 0
+            z01.PrintRune('o')   //принтится А
+        }
+        for i := 1; i <= x-2; i++ {  //цикл запускается если i <= x-2
+            z01.PrintRune('-')
+        }
+        if x > 1 {   //если ширина x > 1
+            z01.PrintRune('o')
+        }
+        if x > 0 {   //если ширина x > 0
+            z01.PrintRune(10)
+        }
 
-		} else {
-			for p := 0; p < x; p++ {
-				if p == 0 || p == x-1 {
-					z01.PrintRune('|')
-				} else {
-					z01.PrintRune(' ')
-				}
-			}
-			z01.PrintRune(10)
-		}
+    }
 
-	}
+    if y > 1 { //если длина  > 1
+        if x > 0 {  //если ширина x > 0
+            for j := 1; j <= y-2; j++ {    //цикл запускается если i <= длина-2
+                z01.PrintRune('|')
+                for i := 1; i <= x-2; i++ {   //цикл запускается если i <= ширина-2
+                    z01.PrintRune(' ')
+                }
+                if x > 1 {
+                    z01.PrintRune('|')
+                }
+                if x > 0 {
+                    z01.PrintRune(10)
+                }
+            }
+        }
+    }
+
+    if y > 1 {
+        if x > 0 {
+            z01.PrintRune('o')       }
+        for i := 1; i <= x-2; i++ {   //цикл запускается если i <= ширина-2
+            z01.PrintRune('-')
+        }
+        if x > 1 {
+            z01.PrintRune('o')       }
+        if x > 0 {
+            z01.PrintRune(10)
+        }
+    }
+}
+
+func main() {
+    Raid1c(5,3)
+    Raid1c(5,1)
+    Raid1c(1,1)
+    Raid1c(1,5)
 }
